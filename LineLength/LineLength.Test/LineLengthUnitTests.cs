594f0f674bd63d669029db3a6c1e3ec99cf6f4be
Dmitrij Lytkin <green-d@outlook.com>
2024-02-09 18:16:29 +0100

    Add project files.

---
+++ LineLength/LineLength.Test/LineLengthUnitTests.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.CodeAnalysis.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+using VerifyCS = LineLength.Test
+    .CSharpAnalyzerVerifier<LineLength.LineLengthAnalyzer>;
+
+namespace LineLength.Test
+{
+    [TestClass]
+    public class LineLengthUnitTests
+    {
+        public class DynamicDataItem
+        {
+            private string code;
+
+            public DynamicDataItem(
+                string testCaseName,
+                string code,
+                params DiagnosticResult[] expectedResults)
+            {
+                if (string.IsNullOrWhiteSpace(testCaseName))
+                {
+                    throw new ArgumentException(
+                        $"'{nameof(testCaseName)}' cannot be null " +
+                        $"or whitespace.",
+                        nameof(testCaseName));
+                }
+
+                TestCaseName = testCaseName;
+                Code = code;
+                ExpectedResults.AddRange(expectedResults);
+            }
+
+            public string TestCaseName { get; }
+
+            public string Code
+            {
+                get => code;
+                set => code = value ?? string.Empty;
+            }
+
+
+            public List<DiagnosticResult> ExpectedResults { get; }
+                = new List<DiagnosticResult>();
+
+
+            public static implicit operator object[](DynamicDataItem item)
+            {
+                var rv = new object[]
+                {
+                    item.Code,
+                    item.ExpectedResults
+                };
+
+                return rv;
+            }
+        }
+
+
+        public static string GetTestCaseName(MethodInfo methodInfo, object[] objects)
+        {
+            var item = objects[0] as DynamicDataItem;
+
+            return item.TestCaseName;
+        }
+
+
+        //No diagnostics expected to show up
+        [DataTestMethod()]
+        [DynamicData(nameof(DynamicData),
+            DynamicDataSourceType.Method,
+            DynamicDataDisplayName = nameof(GetTestCaseName))]
+        public async Task LineLengthCheckTest(
+            DynamicDataItem item)
+        ////string code,
+        ////params DiagnosticResult[] expectedResults)
+        {
+            await VerifyCS.VerifyAnalyzerAsync(
+                item.Code,
+                item.ExpectedResults.ToArray());
+        }
+
+
+        public static IEnumerable<object[]> DynamicData()
+        {
+            const string Comment80 =
+                "//23456789qwertyuiop" +
+                "0123456789qwertyuiop" +
+                "0123456789qwertyuiop" +
+                "0123456789qwertyuiop";
+
+
+
+            yield return new object[] {
+                new DynamicDataItem("No code", "")
+            };
+            yield return new object[] {
+                new DynamicDataItem("81 chars",
+                    Comment80 + "=",
+                    new DiagnosticResult(LineLengthAnalyzer.LinesLengthRule)
+                        .WithLocation(1,1))
+            };
+            yield return new object[]
+            {
+                new DynamicDataItem("81+80+81 chars",
+                @$"{Comment80}1
+{Comment80}
+{Comment80}2",
+                    new DiagnosticResult(LineLengthAnalyzer.LinesLengthRule)
+                        .WithLocation(1,1),
+                    new DiagnosticResult(LineLengthAnalyzer.LinesLengthRule)
+                        .WithLocation(3,1))
+            };
+        }
+
+    }
+}
---
+++ LineLength/LineLength/LineLengthAnalyzer.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+namespace LineLength
+{
+    [DiagnosticAnalyzer(
+        LanguageNames.CSharp,
+        LanguageNames.VisualBasic,
+        LanguageNames.FSharp)]
+    public class LineLengthAnalyzer : DiagnosticAnalyzer
+    {
+        public const string LengthDiagnosticId = "LK0080";
+
+
+        private static readonly LocalizableString LengthTitle
+            = new LocalizableResourceString(
+                nameof(Resources.LengthAnalyzerTitle),
+                Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString LengthMessageFormat
+            = new LocalizableResourceString(
+                nameof(Resources.LengthAnalyzerMessageFormat),
+                Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString LengthDescription
+            = new LocalizableResourceString(
+                nameof(Resources.LengthAnalyzerDescriptor),
+                Resources.ResourceManager, typeof(Resources));
+
+        private const string LengthCategory = "LeetKean.CodeCulture";
+
+        internal static readonly DiagnosticDescriptor LinesLengthRule
+            = new DiagnosticDescriptor(
+                LengthDiagnosticId,
+                LengthTitle,
+                LengthMessageFormat,
+                LengthCategory,
+                DiagnosticSeverity.Warning,
+                isEnabledByDefault: true,
+                description: LengthDescription);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                return ImmutableArray.Create(
+                    LinesLengthRule);
+            }
+        }
+
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            context.RegisterSyntaxTreeAction(ctx =>
+            {
+                AnalyzeLinesLength(ctx.Tree, ctx.ReportDiagnostic);
+            });
+        }
+
+
+        private static void AnalyzeLinesLength(SyntaxTree tree,
+            Action<Diagnostic> reportDiagnostic)
+        {
+            var txt = tree.GetText();
+            var diags = new List<Diagnostic>();
+
+            foreach (var line in txt.Lines)
+            {
+                if ((line.End - line.Start) > 80)
+                {
+                    var loc = Location.Create(tree, line.Span);
+                    diags.Add(Diagnostic.Create(LinesLengthRule, loc));
+                }
+            }
+
+            foreach (var diag in diags)
+            {
+                reportDiagnostic(diag);
+            }
+        }
+
+    }
+}
